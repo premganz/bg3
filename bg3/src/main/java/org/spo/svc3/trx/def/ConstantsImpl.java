1b781470c9e88706e34488795b3bcb7accb2191a
pg <misterprem@gmail.com>
2016-04-23 21:53:25 +0530

    constants commented

--- bg3/src/main/java/org/spo/svc3/trx/def/ConstantsImpl.java
+++ bg3/src/main/java/org/spo/svc3/trx/def/ConstantsImpl.java
@@ -2,7 +2,7 @@ package org.spo.svc3.trx.def;
 
 import org.spo.ifs3.template.web.Constants;
 import org.springframework.stereotype.Component;
-@Component
+//@Component
 public class ConstantsImpl implements Constants {
 
 	public String getRepoPath() {
