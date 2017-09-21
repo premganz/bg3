72d6a9492dd6ef71c19948d04f05eaa90c68f9a6
kv <kv@kv-HP>
2017-09-22 01:33:52 +0530

    pg-rollback to 1.4.9

---
+++ bg3/src/main/java/org/spo/svc2/trxdemo/pgs/c01/task/C0102.java
@@ -0,0 +1,37 @@
+package org.spo.svc2.trxdemo.pgs.c01.task;
+
+import org.spo.ifs2.dsl.controller.NavEvent;
+import org.spo.ifs2.dsl.controller.TrxInfo;
+import org.spo.ifs2.dsl.model.AbstractTask;
+import org.springframework.stereotype.Component;
+@Component
+public class C0102 extends AbstractTask {
+
+	@Override
+	public NavEvent initView(TrxInfo info) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NavEvent initTask(String dataId, TrxInfo info) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NavEvent processViewEvent(String event, TrxInfo info) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String processAjaxEvent(String event, TrxInfo info) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+		
+	
+}
