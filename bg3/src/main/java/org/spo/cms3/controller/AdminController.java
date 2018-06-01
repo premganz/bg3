6259c5e72bd5bab35494629563385c2c5e07dd7b
pg <misterprem@gmail.com>
2018-06-01 13:58:59 +0530

    fx

--- bg3/src/main/java/org/spo/cms3/controller/AdminController.java
+++ bg3/src/main/java/org/spo/cms3/controller/AdminController.java
@@ -35,6 +35,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
